a0a8390a4677669f291ba357dd5e9e7c7ce919fb
Reneicko <rkgetten@gmail.com>
2018-03-13 15:15:17 -0400

    added list and add functions

--- api/controllers/OpenarticlesController.js
+++ api/controllers/OpenarticlesController.js
@@ -8,7 +8,34 @@
 module.exports = {
     list:function(req,res)
     {
-        res.view('list')
+        Openarticles.find({}).exec(function(err,articles){
+if(err)
+{
+    res.send(500,{error:'Db erro'});
+}
+res.view('list',{articles:articles});
+        })
+        //res.view('list')
+    },
+    add:function(req,res)
+    {
+        res.view('add');
+    },
+    create:function(req,res)
+    {
+        var title=req.body.title;
+        var body=req.body.content;
+
+        Openarticles.create({Title:title,body:body}).exec(function(err){
+if(err)
+{
+    res.send(500,{error:'DB error'})
+}
+res.redirect('/openarticles/list')
+        })
     }
+
+    
 };
+
 
